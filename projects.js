783e59dde72513205fe92523d93e182357fbd83b
Risingvirtue <risingvirtue@berkeley.edu>
2018-04-22 13:17:55 -0700

    transition

--- projects.js
+++ projects.js
@@ -23,16 +23,28 @@ function resize() {
 
 function selectButton(type) {
 	console.log(type);
+	
 	if (type != active) {
-		$('#' + active).toggleClass('active');
-		$('#' + type).toggleClass('active');
+		$('#' + active + ' > .top').css('left', '100%');
+		$('#' + active + ' > .top').css('opacity', '0');
+		$('#' + active + ' > .bottom').css('opacity', '0');
+		$('#' + active + ' > .bottom').css('right', '100%');
+		
+		$('#' + type + ' > .top').css('left', '0px');
+		$('#' + type + ' > .top').css('opacity', '1');
+		$('#' + type + ' > .bottom').css('opacity', '1');
+		$('#' + type + ' > .bottom').css('right', '0px');
 		active = type;
 	}
-	$('#right').css('left', '0px');
-	$('#left').css('right', '0px');
+	
+	//$('#right').css('left', '0px');
+	//$('#left').css('right', '0px');
+	//$('#loading-text').css('opacity', '1');
 	setTimeout(function() {
-		$('#right').css('left', '100%');
-		$('#left').css('right', '100%');
+	//	$('#loading-text').css('opacity', '0');
+		//$('#right').css('left', '100%');
+		//$('#left').css('right', '100%');
+		
 	}, 1000)
 	//$('.javascript').css({height: 0, width: 0, opacity: "0"});
 }
